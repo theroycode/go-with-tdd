ae60bc89db2cbacc9d15d3ccc771ebee63858f7e
Rohan Roy <176316665+theroycode@users.noreply.github.com>
2025-04-24 18:42:21 +0530

    learnt struct, methods, interfaces and table based tests

---
+++ struct-methods-interfaces/shapes.go
@@ -0,0 +1,41 @@
+package main
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
